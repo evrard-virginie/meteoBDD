9bff2aab74d39085b620b9708179a902f426f8d2
Virgin-ie <ninie.devlp@gmail.com>
2022-11-15 15:48:02 +0100

    test sur restore.py, backup.py avec pgdump

--- etude/database/backups/backup.py
+++ etude/database/backups/backup.py
@@ -1,35 +1,77 @@
-# Sauvegarde de la base de données
-# utilistaion de pg_dump et pg_dumpall car ils sont complémentaires
-# pg_dump -> tables, champs, séquences, etc
-# pg_dumpall -> backup intégral de l'instance, y compris les users
+# Lancer le programme : Run -> backup OU Terminal C:\Users\...\backups> python backup.py
+import psycopg2
+from psycopg2 import errors
+import os
+from config import config  # a partir du fichier de configuration importer la fonction de configuration (config.py)
+from datetime import date
+import subprocess
 
-# Options :
-# -h : hostname          -p : port
-# -d : database          -U : user
+# Connexion au serveur de la bdd PostgreSQL
+def connect():
+    connection = None   # None = ne retourne aucune valeur
 
-# -f : fichier de sortie                                    -z : niveau de compression (5 par défaut)
-# -F : précision du format de sortie                        -a : datas sans la structure
-#   c : custom (binaire)                                    -n : préciser un schéma particulier
-#   d : directory (permet la parallélisation)               -N : préciser un schéma à exclure
-#   t : tar                                                 -c : inclure le create database
-#   p : plain text (format sql visible directement)
+    try:
+        # récupère les paramètres de connexion
+        params = config()
 
-# -j : parallélisation
+        print('Connexion a la base de donnees Postgresql ..')
 
-# ##### simple export d'une database
-# pg_dump base_de_donnees > dumps_base_de_donnees.sql
-# ##### ou
-# pg_dump -f dump_base_de_donnees.sql -d base_de_donnees
-# ##### compression avec le pipe
-# pg_dump base_de_donnees | gzip -c > dump.base_de_donnees.sql.gz
-# ##### en mode custom (binaire)
-# pg_dump -Fc base_de_donnees -f dump_base_de_donnees_fc.dmp
-# ##### séparation des datas et de la structure
-# pg_dump -Fc base_de_donnees --datas-only -f dump_base_de_donnees_fc.dmp
-# pg_dump --schema-only -f dump_base_de_donnees.sql -d base_de_donnees
+        # double astérisque(quarks) permet d'extraire tout les paramètres d'un dictionnaire.
+        # le dictionnaire db du fichier config.py qui stocke les paramètres de connexion
+        # ça permet la connexion en utilisant le module de psycopg2
+        connection = psycopg2.connect(**params)
 
-# Datas
-# C:\Program Files\PostgreSQL\13\bin>pg_dump -U dev -W -a -Fp application_meteo_db > C:\Users\Virginie\Envs\meteoBDD\etude\database\backups\nom_de_mon_fichier.dmp
+        # creer un curseur pour écouter les requetes
+        cur = connection.cursor()
+        print('Version de la base de données PostgreSQL : ')
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()   # fetchone = 1 résultat
+        print(db_version)
 
-# Structure
-# C:\Program Files\PostgreSQL\13\bin>pg_dump -U dev -W --schema-only -Fp application_meteo_db > C:\Users\Virginie\Envs\meteoBDD\etude\database\backups\nom_de_mon_fichier.sql
+        # connaître la date de la sauvegarde
+        date_backup = date.today()
+        # transforme la date en string pour pouvoir l'afficher avec le nom du fichier, et les tirets en points
+        str_date_backup = str(date_backup).replace('-',('.'))
+
+        # Chemin d'entrée et de sortie
+        BACKUP_DIR = "C:\\Users\\Virginie\\Envs\\meteoBDD\\etude\\database\\backups\\scripts_backups\\"
+        path_input = "C:\\Program Files\\PostgreSQL\\13\\bin"
+
+        # Configuration spécifique pour Windows
+        os.chdir(path_input)
+        # os.putenv('PGPASSWORD', PGPASSWORD)
+        # os.environ["password"] = password
+        print("Répertoire de travail redéfinit : " + os.getcwd())
+
+        # Options :
+        # -h : hostname          -p : port      -W : password
+        # -d : database          -U : user
+
+        filename = str_date_backup + "_backup.dmp"
+        dump = ".\pg_dump -U dev -W -a -Fp application_meteo_db"
+        # command =  "> " + dump + " > " + BACKUP_DIR + filename
+        command = "{0}> pg_dump -U dev -W -a -Fp application_meteo_db {1} > {2}".format(path_input, BACKUP_DIR, filename)
+
+        # r'C:\Users\Virginie\Envs\meteoBDD\etude\database\backups\scripts_backups' + '\\' + str_date_backup + '_backup_data_bdd.dmp'
+
+        # subprocess.call(command, shell=True)
+
+        print(command)
+        os.system(command)
+
+        # fermer le curseur
+        cur.close()
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            # fermer toutes les connexions
+            connection.close()
+            print('Connexion à la base de données terminée')
+
+if __name__ == "__main__":
+    connect()
+
+
+
--- etude/database/backups/restore.py
+++ etude/database/backups/restore.py
@@ -1,1 +1,6 @@
 # script python pour la restauration de la base de données
+
+# Simple restauration de bdd
+# psql db_name < db_backup.sql
+# ##### ou
+#psql -f cluster_backup.sql postgres
